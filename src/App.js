81d93e3fff9945252f1b4c63165a8009db55cf17
Galib77 <razagalib7@gmail.com>
2020-11-10 23:32:55 +0530

    banner section completed

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 
 // components
 import Header from './components/navbar/Header'
+import Banner from './components/banner/Banner'
+import Title from './components/title/Title'
 
 const App = () => {
   return (
     <div className="App">
       <Header />
-     
+     <Banner />
+     <Title />
     </div>
   );
 }
